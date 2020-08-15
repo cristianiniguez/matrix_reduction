ff88c2e9a43edb30e4a8f4c5e4d9da54f98d0785
Cristian Felix Iñiguez Porcel <felizpc1@gmail.com>
2020-08-15 14:04:37 -0400

    archivos principales

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    // module: {
+    //     rules: [
+    //         {
+    //             test: /\.html$/i,
+    //             loader: 'html-loader',
+    //         },
+    //     ],
+    // },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [{
+                from: './src/styles/styles.css',
+                to: ''
+            }],
+        })
+    ]
+}
